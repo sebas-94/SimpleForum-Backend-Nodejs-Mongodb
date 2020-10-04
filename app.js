9f40a63d2901329952a607f999b753c2ae9ba535
sebas-94 <sebas.solis.94@gmail.com>
2020-10-04 22:33:07 +0200

    fix controllers, postman collections and CORS

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 
 // Routers overwrites
@@ -26,8 +33,8 @@ app.use('/api', comment_routes);
 
 // Test route
 app.get('/prueba', (req, res) => {
-    return res.status(200).send({ 
-        message: 'Hola mundo' 
+    return res.status(200).send({
+        message: 'Hola mundo'
     });
 })
 
--- routes/comment.js
+++ routes/comment.js
@@ -14,5 +14,7 @@ router.get('/testComment', CommentController.test);
 router.post('/comment/topic/:topicId', md_auth.authenticated, CommentController.add);
 router.put('/comment/:commentId', md_auth.authenticated, CommentController.update);
 router.delete('/comment/:topicId/:commentId', md_auth.authenticated, CommentController.delete);
+router.get('/search/:search', CommentController.search);
+
 
 module.exports = router;
